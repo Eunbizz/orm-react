c7e36e89c0f0e9166419e492078aa2c3ae08dda2
Eunbizz <eb001020@naver.com>
2024-02-28 16:16:48 +0900

    실습

--- DAY013/react-bootstrap-app/src/pages/ArticleRegist.js
+++ DAY013/react-bootstrap-app/src/pages/ArticleRegist.js
@@ -6,8 +6,35 @@ const ArticleRegist = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>regist</h1>
+    <div className="container-xl">
+      <h2>게시글 작성</h2>
+      <form>
+        <div className="mb-3">
+          <label for="postTitle" className="form-label">
+            제목
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="postTitle"
+            placeholder="제목을 입력하세요"
+          />
+        </div>
+        <div className="mb-5">
+          <label for="postContent" className="form-label">
+            내용
+          </label>
+          <textarea
+            className="form-control"
+            id="postContent"
+            rows="20"
+            placeholder="내용을 입력하세요"
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          제출
+        </button>
+      </form>
     </div>
   );
 };
---
+++ DAY013/react-chat-app/src/routes/routes.js
@@ -0,0 +1,25 @@
+// 인증 및  비인증 라우팅 규칙들을 2개의 배열로 나눠 구성하고 주소체계 정의하는 모듈
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+// 사용자 인증이 필요한 화면컴포넌트
+const StarterPage = React.lazy(() => import("../pages/Starter/index"));
+const Dashboard = React.lazy(() => import("../pages/Dashboard/index"));
+
+// 사용자 인증이 필요없는 화면컴포넌트
+const Login = React.lazy(() => import("../pages/Auth/Login"));
+const Register = React.lazy(() => import("../pages/Auth/Register"));
+
+// 인증이 필요한 라우팅 목록 및 라우팅 규칙 정의
+const authProtectedRoutes = [
+  { path: "/starter", component: <StarterPage /> },
+  { path: "/dashboard", component: <Dashboard /> },
+  { path: "/", exact: true, component: <Navigate to="/dashboard" /> },
+];
+
+const publicRoutes = [
+  { path: "/signin", component: <Login /> },
+  { path: "/signup", component: <Register /> },
+];
+
+export { authProtectedRoutes, publicRoutes };
